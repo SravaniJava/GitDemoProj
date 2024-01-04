d12c865ba50e12547ce856aba7bd87556ab3214b
SravaniJava <sravanigoudjava@gmail.com>
2024-01-04 10:08:38 +0530

    revert back option eg 2

--- src/SampleForGit.java
+++ src/SampleForGit.java
@@ -7,6 +7,8 @@ public class SampleForGit {
     System.out.println("this is 3rd change");
     System.out.println("is this msg from sravani's bracnh");
         System.out.println("is this msg from 2nd time sravani's bracnh");
+   System.out.println("this is for undo commit");
     demo.test1();
+
     }
 }
